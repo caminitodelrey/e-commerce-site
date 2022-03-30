91cfdcdd802595b00dc70eef24fd33d29dea37f8
yiiiiilin <yilinliu7@gmail.com>
2022-03-30 13:57:22 -0500

    Git flow test

--- client/src/components/ratings-and-reviews/ratings-and-reviews.jsx
+++ client/src/components/ratings-and-reviews/ratings-and-reviews.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export default function RatingsReviews () {
 
   return (
-    <div>RatingsReviews testing</div>
+    <div>RatingsReviews testing test git flow </div>
   )
 }
