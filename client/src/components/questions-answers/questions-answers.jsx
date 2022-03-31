01d14a355b7b9b929965d13be8495a105db2d140
Colin G Fitzhenry <cgfitzhenry@gmail.com>
2022-03-31 11:52:27 -0400

    updated with routes

--- client/src/components/questions-answers/questions-answers.jsx
+++ client/src/components/questions-answers/questions-answers.jsx
@@ -20,7 +20,12 @@ class QuestionsAnswers extends React.Component {
 
   // product_id: 37311 - 38199
 
+
   componentDidMount() {
+    // GET /qa/questions
+    // List Questions
+    // Retrieves a list of questions for a particular product.
+    // This list does not include any reported questions.
     axios({
       method: 'get',
       baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions?product_id=38199',
@@ -31,6 +36,50 @@ class QuestionsAnswers extends React.Component {
     }).then(data => console.log(data)) // Refactor
       .catch(err => console.error(err));
   }
+
+  // GET /qa/questions/:question_id/answers
+  // Answers List
+  // Returns answers for a given question. This list does not include any reported answers.
+
+
+  // POST /qa/questions
+  // Add a Question
+  // Adds a question for the given product
+
+
+  // POST /qa/questions/:question_id/answers
+  // Add an Answer
+  // Adds an answer for the given question
+
+
+  // PUT /qa/questions/:question_id/helpful
+  // Mark Question as Helpful
+  // Updates a question to show it was found helpful.
+
+
+  // PUT /qa/questions/:question_id/report
+  // Report Question
+  // Updates a question to show it was reported.
+  // Note, this action does not delete the question, but the question will not be returned in the above GET request.
+
+
+  // PUT /qa/answers/:answer_id/helpful
+  // Mark Answer as Helpful
+  // Updates an answer to show it was found helpful.
+
+
+  // PUT /qa/answers/:answer_id/report
+  // Report Answer
+  // Updates an answer to show it has been reported.
+  // Note, this action does not delete the answer, but the answer will not be returned in the above GET request.
+
+
+
+
+
+
+
+
 
 
 
