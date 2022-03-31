4c19061a65cbbcd063280faeaafbf492184242ee
Colin G Fitzhenry <cgfitzhenry@gmail.com>
2022-03-30 20:22:53 -0400

    started axios request

--- client/src/components/questions-answers/questions-answers.jsx
+++ client/src/components/questions-answers/questions-answers.jsx
@@ -5,9 +5,23 @@ class QuestionsAnswers extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hello: 'world of mine'
+      product: this.props.selectedProduct
     }
   }
+
+  componentDidMount() {
+    // axios.get()
+  }
+
+  // {
+  //   "id": 1,
+  //   "name": "Camo Onesie",
+  //   "slogan": "Blend in to your crowd",
+  //   "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
+  //   "category": "Jackets",
+  //   "default_price": "140"
+  // },
+
 
   render() {
     return (
