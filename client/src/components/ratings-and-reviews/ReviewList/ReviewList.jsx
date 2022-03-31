33f8579cbccaa4be288a310a06e0a8767f4c0753
yiiiiilin <yilinliu7@gmail.com>
2022-03-31 15:07:30 -0500

    Add DummyData

---
+++ client/src/components/ratings-and-reviews/ReviewList/ReviewList.jsx
@@ -0,0 +1,8 @@
+import React, {useEffect, useState} from 'react';
+
+export default function () {
+
+  return (
+    <div>-[ReactList]</div>
+  )
+};
--- client/src/components/ratings-and-reviews/ratings-and-reviews.jsx
+++ client/src/components/ratings-and-reviews/ratings-and-reviews.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import getData from '../../../helper.js';
+import ReviewList from './ReviewList/ReviewList.jsx';
+import ReviewsExampleData from './DummyData/ReviewsExampleData.jsx';
 
 export default function RatingsReviews () {
 
-
-
+console.log(ReviewsExampleData)
 
   return (
-    <div>[RatingsReviews testing test git flow]</div>
+    <div>[RatingsReviews testing test git flow]
+      <div>
+        <ReviewList />
+      </div>
+    </div>
 
   )
 }
