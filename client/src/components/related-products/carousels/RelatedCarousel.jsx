4b3815c43580d38db593c386568d12e60327dca1
Haley Jung <haleyjung16@gmail.com>
2022-04-04 13:36:59 -0400

    add handleKeyDown to close modal when escape key is pressed

--- client/src/components/related-products/carousels/RelatedCarousel.jsx
+++ client/src/components/related-products/carousels/RelatedCarousel.jsx
@@ -1,102 +1,118 @@
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
+
 import React, { useState, useEffect } from 'react';
 import Ratings from '../subcomponents/Ratings.jsx';
 import ProductImg from '../subcomponents/ProductImg.jsx';
 import ComparisonModal from '../subcomponents/ComparisonModal.jsx';
 import ActionButton from '../subcomponents/ActionButton.jsx';
 
-import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import {
+  CardsContainer,
+  CardsWrapper,
+  ContentWrapper,
+  Content,
+  LeftChevron,
+  RightChevron,
+  CardContainer,
+  CardAssetContainer,
+  ProductCategory,
+  ProductName,
+  ProductPrice,
+} from '../../../theme/carouselStyle.js';
 
-import { CardsContainer, CardsWrapper, ContentWrapper, Content, LeftChevron, RightChevron, CardContainer, CardAssetContainer, CardAssetImg, ProductCategory, ProductName, ProductPrice } from '../../../theme/carouselStyle.js';
-
-export default function RelatedCarousel ({ products, mainProduct, show }) {
-
+export default function RelatedCarousel({ products, mainProduct, show }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [length, setLength] = useState(products.length);
   const [modal, setModal] = useState({
     display: false,
-    clickedProduct: {}
+    clickedProduct: {},
   });
 
   const { display, clickedProduct } = modal;
 
-  //set the length to match current children from props
+  // set the length to match current children from props
   useEffect(() => {
-    setLength(products.length)
-  }, [products])
+    setLength(products.length);
+  }, [products]);
 
   const toggleModal = (product) => {
     setModal({
       display: !display,
-      clickedProduct: product
+      clickedProduct: product,
     });
-  }
+  };
 
   const next = () => {
-    if (currentIndex < (length - (show - 1))) {
-      setCurrentIndex(prevState => prevState + 1)
+    if (currentIndex < length - (show - 1)) {
+      setCurrentIndex((prevState) => prevState + 1);
     }
-  }
+  };
 
   const prev = () => {
     if (currentIndex > 0) {
-      setCurrentIndex(prevState => prevState - 1)
+      setCurrentIndex((prevState) => prevState - 1);
     }
-  }
+  };
 
-  const Card = products.map((product) =>
-    <CardContainer
-      key={product.name}
-      style={{width: `calc(100% / ${show})`}}
-    >
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 27) {
+      toggleModal();
+    }
+  };
+
+  const Card = products.map((product) => (
+    <CardContainer key={product.name} style={{ width: `calc(100% / ${show})` }}>
       <CardAssetContainer>
-        <div className='product-card__img' onClick={() => toggleModal(product)}>
-          <ProductImg
-            product={product}
-            mainProduct={mainProduct}
-          />
+        <div
+          className="product-card__img"
+          onClick={() => toggleModal(product)}
+          onKeyDown={handleKeyDown}
+          role="button"
+          tabIndex="0"
+        >
+          <ProductImg product={product} mainProduct={mainProduct} />
         </div>
-        <ActionButton product={product}/>
+        <ActionButton product={product} />
       </CardAssetContainer>
 
-      <div className='product-card__details'>
-        <ProductCategory className='product-card__category'>
+      <div className="product-card__details">
+        <ProductCategory className="product-card__category">
           {product.category.toUpperCase()}
         </ProductCategory>
-        <ProductName className='product-card__name'>
-          {product.name}
-        </ProductName>
-        <ProductPrice className='product-card__price'>
-          ${product.price.replace(/\.00$/,'')}
+        <ProductName className="product-card__name">{product.name}</ProductName>
+        <ProductPrice className="product-card__price">
+          $
+          {product.price.replace(/\.00$/, '')}
         </ProductPrice>
-        <p className='product-card__rating'>
+        <p className="product-card__rating">
           <Ratings />
         </p>
       </div>
-
     </CardContainer>
-  );
+  ));
 
   return (
-
     <CardsContainer>
       <CardsWrapper>
-        {
-          currentIndex > 0 &&
-          <LeftChevron className='left-arrow' onClick={prev}>
+        {currentIndex > 0 && (
+          <LeftChevron className="left-arrow" onClick={prev}>
             <FaChevronLeft />
           </LeftChevron>
-        }
-          <ContentWrapper>
-            <Content style={{transform: `translateX(-${currentIndex * (100 / (show + 1))}%)`}}>
-              {Card}
-            </Content>
-          </ContentWrapper>
-        {
-          currentIndex < (length - (show - 1)) &&
-          <RightChevron className='right-arrow' onClick={next}>
+        )}
+        <ContentWrapper>
+          <Content
+            style={{
+              transform: `translateX(-${currentIndex * (100 / (show + 1))}%)`,
+            }}
+          >
+            {Card}
+          </Content>
+        </ContentWrapper>
+        {currentIndex < length - (show - 1) && (
+          <RightChevron className="right-arrow" onClick={next}>
             <FaChevronRight />
           </RightChevron>
-        }
+        )}
       </CardsWrapper>
 
       {display && (
--- client/src/components/related-products/subcomponents/ComparisonModal.jsx
+++ client/src/components/related-products/subcomponents/ComparisonModal.jsx
@@ -1,107 +1,102 @@
 import React from 'react';
+import { FcCheckmark } from 'react-icons/fc';
+import {
+  ModalContainer,
+  ModalTitle,
+  ModalProductName,
+  ModalContent,
+  ModalBody,
+  TableRow,
+} from '../../../theme/modalStyle.js';
 import getData from '../../../../helper.js';
 
-import { FcCheckmark } from "react-icons/fc";
-import { ModalContainer, ModalTitle, ModalProductName, ModalContent, ModalBody, TableRow } from '../../../theme/modalStyle.js';
+export default function ComparisonModal({ toggleModal, product, mainProduct }) {
+  // getFeatures() {
+  //   Promise.all([
+  //     getData(`products/${this.props.mainProduct.id}`),
+  //     getData(`products/${this.props.product.id}`),
+  //   ])
+  //     .then((data) => {
+  //       // let mainProductFeature = [...this.state.mainProductFeature];
+  //       // let productFeature = [...this.state.productFeature];
 
-export default class ComparisonModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      features: [], // combined features and values
-      mainProductFeature: [],
-      productFeature: []
-    }
-  }
+  //       // mainProductFeature.push(data[0].data.features);
+  //       // productFeature.push(data[1].data.features);
 
-  getFeatures() {
-    Promise.all([
-      getData(`products/${this.props.mainProduct.id}`),
-      getData(`products/${this.props.product.id}`)
-    ])
-      .then(data => {
-        // let mainProductFeature = [...this.state.mainProductFeature];
-        // let productFeature = [...this.state.productFeature];
+  //       // this.setState({
+  //       //   mainProductFeature: mainProductFeature,
+  //       //   productFeature: productFeature
+  //       // })
 
-        // mainProductFeature.push(data[0].data.features);
-        // productFeature.push(data[1].data.features);
+  //       let features = [...this.state.features];
 
-        // this.setState({
-        //   mainProductFeature: mainProductFeature,
-        //   productFeature: productFeature
-        // })
+  //       let mainProductFeatures = data[0].data.features;
+  //       let relatedProductFeatures = data[1].data.features;
+  //       let allFeatures = {};
 
-        let features = [...this.state.features];
+  //       for (let i = 0; i < relatedProductFeatures.length; i += 1) {
+  //         let comparisonFeatureObj = {
+  //           feature: relatedProductFeatures[i].feature,
+  //           relatedValue: relatedProductFeatures[i].value,
+  //         };
+  //         mainProductFeatures.forEach((featureObj) => {
+  //           if (featureObj.feature === comparisonFeatureObj.feature) {
+  //             comparisonFeatureObj.mainValue = comparisonFeatureObj.relatedValue;
+  //           }
+  //         });
 
-        let mainProductFeatures = data[0].data.features;
-        let relatedProductFeatures = data[1].data.features;
-        let allFeatures = {};
+  //         comparisonFeatureObj.mainValue = comparisonFeatureObj.mainValue || '';
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.error('getFeatures Error:', err);
+  //     });
+  // }
 
-        for (let i = 0; i < relatedProductFeatures.length; i++) {
-          let comparisonFeatureObj = {
-            feature: relatedProductFeatures[i].feature,
-            relatedValue: relatedProductFeatures[i].value
-          };
-          mainProductFeatures.forEach(featureObj => {
-            if(featureObj.feature === comparisonFeatureObj.feature) {
-              comparisonFeatureObj['mainValue'] = comparisonFeatureObj.relatedValue;
-            }
-          });
+  // componentDidMount() {
+  //   this.getFeatures();
+  // }
 
-          comparisonFeatureObj['mainValue'] = comparisonFeatureObj['mainValue'] || '';
-
-        }
-
-
-
-
-      })
-      .catch(err => {
-        console.error('getFeatures Error:', err);
-      })
-  }
-
-  componentDidMount() {
-    this.getFeatures();
-  }
-
-  render() {
-    return (
-      <ModalContainer onClick={this.props.toggleModal}>
-        <ModalContent onClick={this.props.toggleModal}>
-          <ModalTitle>
-            <p>COMPARING</p>
-          </ModalTitle>
-          <ModalBody>
-            <table>
-              <thead>
-                <tr>
-                  <ModalProductName width='30%'>{this.props.mainProduct.name}</ModalProductName>
-                  <th></th>
-                  <ModalProductName width='30%'>{this.props.product.name}</ModalProductName>
-                </tr>
-              </thead>
-              <tbody>
-
-
-                <TableRow>
-                  <td><FcCheckmark /></td>
-                  <td>GMO and Pesticide-free</td>
-                  <td><FcCheckmark /></td>
-                </TableRow>
-                <TableRow>
-                  <td><FcCheckmark /></td>
-                  <td>Made with 100% Genetic Modification</td>
-                  <td></td>
-                </TableRow>
-
-
-              </tbody>
-            </table>
-          </ModalBody>
-        </ModalContent>
-      </ModalContainer>
-    )
-
-  }
+  return (
+    <ModalContainer onClick={toggleModal}>
+      <ModalContent onClick={toggleModal}>
+        <ModalTitle>
+          <p>COMPARING</p>
+        </ModalTitle>
+        <ModalBody>
+          <table>
+            <thead>
+              <tr>
+                <ModalProductName width="30%">
+                  {mainProduct.name}
+                </ModalProductName>
+                <th> </th>
+                <ModalProductName width="30%">
+                  {product.name}
+                </ModalProductName>
+              </tr>
+            </thead>
+            <tbody>
+              <TableRow>
+                <td>
+                  <FcCheckmark />
+                </td>
+                <td>GMO and Pesticide-free</td>
+                <td>
+                  <FcCheckmark />
+                </td>
+              </TableRow>
+              <TableRow>
+                <td>
+                  <FcCheckmark />
+                </td>
+                <td>Made with 100% Genetic Modification</td>
+                <td> </td>
+              </TableRow>
+            </tbody>
+          </table>
+        </ModalBody>
+      </ModalContent>
+    </ModalContainer>
+  );
 }
