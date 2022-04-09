57a28598427cffe09601a90803c5db24053d594f
Haley Jung <haleyjung16@gmail.com>
2022-04-09 05:06:05 -0400

    add styled header and banner slider

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,36 +1,92 @@
 import { MdLocalShipping } from 'react-icons/md';
+import { FiHeart, FiShoppingBag } from 'react-icons/fi';
+
 import React, { useState, useEffect } from 'react';
-import { HeaderOuter, HeaderInner, Slideshow, BannerSlider } from '../../theme/headerStyle.js';
+import {
+  HeaderOuter,
+  HeaderInner,
+  Logo,
+  Nav,
+  ThirdInnerContainer,
+  Search,
+  WishlistButton,
+  ShopButton,
+  Slideshow,
+  BannerSlider,
+  Slide,
+  BannerText,
+  BannerLink,
+} from '../../theme/headerStyle.js';
+
+const banner1 = (
+  <Slide>
+    <BannerText>
+      Free Shipping + Returns, Free Membership, Exclusive Products
+    </BannerText>
+    <BannerLink>Join Now</BannerLink>
+  </Slide>
+);
+
+const banner2 = (
+  <Slide>
+    <BannerText>Save Up to 40%</BannerText>
+    <BannerLink>Shop All Our New Markdowns</BannerLink>
+  </Slide>
+);
 
 export default function Header() {
+  const banners = [banner1, banner2];
+  const delay = 2500;
 
-  const banner1 = (
-    <div>
-      <p>Free Shipping + Returns, Free Membership, Exclusive Products</p>
-      <p>Join Now</p>
-    </div>
-  );
+  const [index, setIndex] = useState(0);
 
-  const banner2 = (
-    <div>
-      <p>Save Up to 40%</p>
-      <p>Shop All Our New Markdowns</p>
-    </div>
-  );
+  useEffect(() => {
+    setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === banners.length - 1 ? 0 : prevIndex + 1,
+        ),
+      delay,
+    );
+    return () => {};
+  }, [index]);
 
   return (
     <>
       <HeaderOuter>
         <HeaderInner>
-          Logo, Dark/Light Mode Toggle, Search bar
+          <Logo>
+            <h1>CAMINITO</h1>
+          </Logo>
+
+          <div>
+            <Nav>New Releases</Nav>
+            <Nav>Men</Nav>
+            <Nav>Women</Nav>
+            <Nav>Kids</Nav>
+          </div>
+
+          <ThirdInnerContainer>
+            <Search>
+              <input type="text" placeholder="Search" />
+            </Search>
+
+            <div>
+              <WishlistButton />
+              <ShopButton />
+            </div>
+          </ThirdInnerContainer>
+
         </HeaderInner>
       </HeaderOuter>
 
       <Slideshow>
-          <BannerSlider>
-            {banner1}
-          </BannerSlider>
+        <BannerSlider
+          style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+        >
+          {banners.map((banner) => banner)}
+        </BannerSlider>
       </Slideshow>
     </>
-  )
+  );
 }
--- client/src/theme/headerStyle.js
+++ client/src/theme/headerStyle.js
@@ -1,50 +1,133 @@
 import styled from 'styled-components';
+import { FiHeart, FiShoppingBag } from 'react-icons/fi';
 
 // Shrinking Header
 export const HeaderOuter = styled.div`
   display: flex;
   align-items: center;
-  top: -50;
-  height: 120px;
+  justify-content: space-between;
+  top: -30;
+  height: 100px;
   width: 100%;
   background-color: #fff;
+  color: rgb(25, 46, 64);
 `;
 
-export const HeaderInner = styled.div`
-  display: flex;
-  align-items: center;
+export const HeaderInner = styled(HeaderOuter)`
   position: sticky;
   top: 0;
   height: 70px;
-  width: 100%;
-  background-color: #fff;
+  padding: 5px 30px;
 `;
 
+export const Logo = styled.div`
+`;
+
+export const Nav = styled.p`
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 30px;
+  margin-left: 30px;
+  font-weight: 600;
+  cursor: pointer;
+
+  &:hover {
+    color: rgb(3, 115, 83);
+  }
+`;
+
+export const ThirdInnerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Search = styled.div`
+  position: absolute;
+  right: 120px;
+
+  > input {
+    padding: 12px;
+    outline: none;
+    border: none;
+    border-radius: 20px;
+    background-color: rgb(245, 245, 245);
+    font-size: .9em;
+    color: rgb(3, 115, 83);
+
+    ::placeholder {
+      color: rgb(200, 200, 200);
+    }
+
+    &:hover {
+      background-color: rgb(229, 229, 229);
+      ::placeholder {
+        color: rgb(120, 120, 120);
+      }
+    }
+  }
+`;
+
+export const WishlistButton = styled(FiHeart)`
+  color: rgb(25, 46, 64);
+  width: 20px;
+  height: 20px;
+  padding: 0 20px 0 110px;
+
+  &:hover {
+    color: rgb(25, 46, 64);
+    fill: rgba(11, 191, 125, .7);
+    cursor: pointer;
+  }
+`;
+
+export const ShopButton = styled(FiShoppingBag)`
+  color: rgb(25, 46, 64);
+  width: 20px;
+  height: 20px;
+
+  &:hover {
+    color: rgb(25, 46, 64);
+    fill: rgba(11, 191, 125, .7);
+    cursor: pointer;
+  }
+`;
+
+// Banner Slider
 export const Slideshow = styled.div`
   top: 50px;
   height: 58px;
   width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(0, 0, 0, .2);
   overflow: hidden;
   margin: 0 auto;
+  background-color: rgb(245, 245, 245);
 `;
 
 export const BannerSlider = styled.div`
-  top: 50px;
-  height: 58px;
+  white-space: nowrap;
+  transition: ease 1000ms;
+`;
+
+export const Slide = styled.div`
   width: 100%;
-  display: flex;
+  display: inline-block;
   align-items: center;
   justify-content: center;
-  background-color: rgba(0, 0, 0, .2)
 `;
 
 export const BannerText = styled.p`
   text-align: center;
-  line-height: 1.25;
+  line-height: .5;
   color: black;
-  /* font-size: .9em; */
+  font-weight: 700;
+  font-size: .9em;
+`;
+
+export const BannerLink = styled.p`
+  text-align: center;
+  line-height: .5;
+  color: black;
+  font-size: .7em;
+  text-decoration: underline;
+  cursor: pointer;
 `;
